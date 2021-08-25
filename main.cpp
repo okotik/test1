234771d604bacbacf653f9a6eb15f6b7a3f3155c
Nata0110 <87442131+Nata0110@users.noreply.github.com>
2021-08-25 20:41:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string.h>
 
 void swap(const char *text)
 {
     //please swap the text
     for (int i = 0; i < strlen(text); ++i)
-    {
+
         std::cout << text[i];
-    }
+        std::cout << '\n';
+
+    for (int j = strlen(text); j >= 0; --j )
+        std::cout << text[j];
+
 }
 
 int main()
