af1dc275009e285ad07543bb17d3dd053ecf7008
Orest Hui <ext-orest.hui@here.com>
2021-08-25 20:42:48 +0300

    My lucky rob

--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 
 void swap(const char *text)
 {
-    //please swap the text
+
     for (int i = 0; i < strlen(text); ++i)
-    {
         std::cout << text[i];
-    }
+
+    for (int j = strlen(text); j >= 0; j--)
+        std::cout <<text[j];
 }
+
 
 int main()
 {
-    const char *text = "123456789";
+    const char *text = "taras";
     swap(text);
     return 0;
 }
